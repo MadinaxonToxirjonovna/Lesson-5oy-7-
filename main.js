a603ca3ce0c798e78f988f0bb85772d0c988394c
Madinaxon Toxirjonovna <madinaxontoxirjonovna@gamil.com>
2024-04-29 15:02:30 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// const title = document.createElement("h1");
+// const text = document.createTextNode("Hello Mars");
+// title.appendChild(text)
+// console.log(title);
+
+// const mainCode = document.querySelector("body");
+// mainCode.appendChild(title)
+
+
+// const title = document.getElementsByClassName("hello");
+// console.log(title);
+// title.innerText = "Hello Mars"
+// title.style.color = "red"
+// title[0].innerHTML = "Hello Mars"
+// title[1].innerHTML = "Hello Mars"
+// title[2].innerHTML = "Hello Mars"
+// title[3].innerHTML = "Hello Mars"
+// title[4].innerHTML = "Hello Mars"
+
+// const numbers = [1,2,3,4,5];
+// const double = numbers.map(function(item){
+//     return item * 2;
+// })
+
+// console.log(double);
+
+
+
+//  Amaliy ish 1
+// const numbers = [1, 2, 3, 4, 5];
+
+// const doubled = numbers.map(function (item) {
+//     return item*2;
+// });
+
+// console.log(doubled)
